b2e3e44d8461235cd709b8df3f2a1d0e3c8b1cab
liuyiyuan <liuyiyuan@taqu.cn>
2019-12-11 11:54:35 +0800

    入参改成动态的Array

--- src/main/java/starfish/poi/application/PoiApplication.java
+++ src/main/java/starfish/poi/application/PoiApplication.java
@@ -1,20 +1,20 @@
 package starfish.poi.application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import starfish.poi.domain.WaterMeter;
 import starfish.poi.infrastructure.utils.EasyPoi;
-
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "poi")
 public class PoiApplication {
@@ -28,8 +28,9 @@ public class PoiApplication {
     private EasyPoi easyPoi;
 
     @PostMapping("json2excel")
-    public void JsonParseExcel(@RequestBody List<WaterMeter> waterMeter ,HttpServletResponse response) throws IOException {
-        XSSFWorkbook workbook = easyPoi.createWorkBook(waterMeter);
+    public void JsonParseExcel(@RequestBody List<Map<String,String>> array) throws IOException {
+        XSSFWorkbook workbook = easyPoi.createWorkBook(array);
+        log.info("生成excel......");
         workbook.write(new FileOutputStream(new File(FILE_NAME)));
         workbook.close();
     }
--- src/main/java/starfish/poi/infrastructure/utils/EasyPoi.java
+++ src/main/java/starfish/poi/infrastructure/utils/EasyPoi.java
@@ -3,62 +3,45 @@ package starfish.poi.infrastructure.utils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.xssf.usermodel.*;
 import org.springframework.stereotype.Component;
-import starfish.poi.domain.WaterMeter;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 @Component
 public class EasyPoi {
 
-    private static final List<String> Sheet = Arrays.asList("id","name","time","data","status");
+    private static final Set<String> Sheet = new LinkedHashSet<String>();
 
-    public XSSFWorkbook createWorkBook(List<WaterMeter> waterMeters) {
+    public XSSFWorkbook createWorkBook(List<Map<String,String>> array) {
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("lz");
-
-        log.info("创建表头......");
-        createTitle(workbook, sheet);
-
         //设置单元格数据居中z
         XSSFCellStyle rowStyle = workbook.createCellStyle();
         rowStyle.setAlignment(XSSFCellStyle.ALIGN_CENTER);
 
-        //固定列
+
+        log.info("填充数据......");
         int rowNum = 1;
-        for (WaterMeter waterMeter: waterMeters) {
+        for (Map<String,String> objectMap: array) {
             XSSFRow row = sheet.createRow(rowNum);
-
-            XSSFCell cell0 = row.createCell(0);
-            cell0.setCellValue(waterMeter.getId());// id
-            cell0.setCellStyle(rowStyle);
-
-            XSSFCell cell1 = row.createCell(1);
-            cell1.setCellValue(waterMeter.getName());// name
-            cell1.setCellStyle(rowStyle);
-
-            XSSFCell cell2 = row.createCell(2);
-            cell2.setCellValue(waterMeter.getTime());// time
-            cell2.setCellStyle(rowStyle);
-
-            XSSFCell cell3 = row.createCell(3);
-            cell3.setCellValue(waterMeter.getData());// data
-            cell3.setCellStyle(rowStyle);
-
-            XSSFCell cell4 = row.createCell(4);
-            cell4.setCellValue(waterMeter.getStatus());// status
-            cell4.setCellStyle(rowStyle);
-
+            int fieldIndex = 0;
+            for (Map.Entry<String,String> entry:objectMap.entrySet()){
+                XSSFCell cell = row.createCell(fieldIndex);
+                cell.setCellValue(entry.getValue());
+                Sheet.add(entry.getKey());
+                cell.setCellStyle(rowStyle);
+                fieldIndex ++;
+            }
             rowNum++;
         }
+
+        log.info("创建表头......");
+        createTitle(workbook, sheet);
         return workbook;
     }
 
     // 创建表头
     private void createTitle(XSSFWorkbook workbook, XSSFSheet sheet) {
-        XSSFRow row = sheet.createRow(0);
-
         for (int i=0;i<Sheet.size();i++){
             // 设置列宽，setColumnWidth的第二个参数要乘以256，这个参数的单位是1/256个字符宽度
             sheet.setColumnWidth(i, 20 * 256);
@@ -72,10 +55,14 @@ public class EasyPoi {
         style.setFont(font);
 
         XSSFCell cell;
-        for (int i = 0; i < Sheet.size(); i++) {
-            cell = row.createCell(i);
-            cell.setCellValue(Sheet.get(i));
+        Iterator<String> iterable = Sheet.iterator();
+        XSSFRow row = sheet.createRow(0);
+        int filedIndex = 0;
+        while (iterable.hasNext()){
+            cell = row.createCell(filedIndex);
+            cell.setCellValue(iterable.next());
             cell.setCellStyle(style);
+            filedIndex ++;
         }
     }
 }
